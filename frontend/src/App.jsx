8727d5c01be607762b4b882aeebe8703c2b0e3ee
Shehab Bin  Shakib <96295133+proshehab@users.noreply.github.com>
2026-08-20 19:19:24 +0600

    feat: refactor App component to implement routing with Home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <h1 className="text-amber-800 font-bold text-3xl">Rant A Car</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   );
 }
